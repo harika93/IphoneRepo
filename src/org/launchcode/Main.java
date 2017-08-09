e0fbdb1b94d0396f7d26b34d0063c2e175e48a18
AshishDakarapu <30699818+AshishDakarapu@users.noreply.github.com>
2017-08-09 15:55:44 -0500

    ashish

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -4,18 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
         // Ashish
-        System.out.print("hey");
-        System.out.print("welcome here");
-        System.out.print("oy here");
-        System.out.print("this a merge check");
-        System.out.print("branch creation");
-        System.out.print("merge conflict chk");
+        System.out.println("hey");
+        System.out.println("welcome here");
+        System.out.println("oy here");
+        System.out.println("this a merge check");
+        System.out.println("branch creation");
+        System.out.println("merge conflict chk");
+        {
+            int x, y, z;
+            System.out.println("Enter two integers to calculate their sum ");
+            x = 3;
+            y = 2;
+            z = x + y;
+            System.out.println("Sum of entered integers = "+z);
+        }
 
         //Harika
         System.out.print("Hello world");
         System.out.print("heyyy");
         System.out.print("heyyy");
-        //harika
+        //varsha
         System.out.print("updating...");
         System.out.print("heyyy...!!!");
 
