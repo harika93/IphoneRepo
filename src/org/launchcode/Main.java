5175930f0302518edc98ed8844f3ba48af7cafe0
AshishDakarapu <30699818+AshishDakarapu@users.noreply.github.com>
2017-08-09 14:55:24 -0500

    ashish

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -3,13 +3,13 @@ package org.launchcode;
 public class Main {
 
     public static void main(String[] args) {
-	    // Ashish
-	    System.out.print("hey");
-	System.out.print("welcome here");
-	System.out.print("oy here");
-	   //Harika
-    System.out.print("Hello world");
-   System.out.print("heyyy");
-	
+        // Ashish
+        System.out.print("hey");
+        System.out.print("welcome here");
+        System.out.print("oy here");
+        //Harika
+        System.out.print("Hello world");
+        System.out.print("heyyy");
+
     }
 }
